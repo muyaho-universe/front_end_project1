7b78bda7dcae7d3f3983f57d6f80a7ad2097b9dd
muyaho-universe <kimdaesk@gmail.com>
2022-10-29 15:34:31 +0900

    Complete add series

--- myHome/add_ok.php
+++ myHome/add_ok.php
@@ -1,1 +1,28 @@
 <?php
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $goodness= $_POST['goodness'];
+?>
+
+<!DOCTYPE html>
+<html lang=""en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP form</title>
+</head>
+<body>
+<div id="top">
+    <h3>Welcome <?=$name?>!</h3>
+</div>
+<div id="center">
+    <div>이름: <?= $name?></div>
+    <div>나이: <?= $age?></div>
+    <div>성별: <?= $gender?> </div>
+    <div>선행정도: <?= $goodness?></div>
+    
+    
+</div>
+
+</body>
